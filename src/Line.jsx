7b936d66ee4a5f769c6e4fbe7376b820e2ac8eab
Rafael <rsilraf@yahoo.com.br>
2016-02-19 13:01:00 -0200

    Update Line.jsx

--- src/Line.jsx
+++ src/Line.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Line extends Component {
  render() {
-   let values = this.props.values.map((v)=>{
+   let values = this.props.values.map(v=>{
      return <li key={v.id}>{v.date.slice(0,10)} - {v.desc} - R$ {v.value}</li>
    });
    return <div><h3>{this.props.name} - R$ {this.props.total}</h3>
